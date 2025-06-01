d0df3606c5264933d9d7c4010bb40434ee8731f6
RafaelRSoares <rafasoares159@gmail.com>
2025-06-01 16:32:10 -0300

    Update tb_CaminhoDeDados.sv

--- Caminho_de_Dados/tb_CaminhoDeDados.sv
+++ Caminho_de_Dados/tb_CaminhoDeDados.sv
@@ -130,34 +130,34 @@ module TB_registradores;
 
     // Apenas para simulação: mostra o valor do clock ao longo do tempo
     always_ff @(posedge clk)begin
-        // $display("========================================================================");
-        // $display("Memoria de Intrucao:");
-        // $display("Tempo: %d |PC: %d", $time,pc);
-        // $display("Instrucao: %b |Opcode: %b |Rs1: %d |Rs2: %d",instruction,Opcode,Rs1,Rs2);
-        // $display("RD: %d |Funct3: %b |Funct7: %b\n",RsD,funct3,funct7);
+        $display("========================================================================");
+        $display("Memoria de Intrucao:");
+        $display("Tempo: %d |PC: %d", $time,pc);
+        $display("Instrucao: %b |Opcode: %b |Rs1: %d |Rs2: %d",instruction,Opcode,Rs1,Rs2);
+        $display("RD: %d |Funct3: %b |Funct7: %b\n",RsD,funct3,funct7);
 
-        // $display("Controle:");
-        // $display("Branch %d |MemRead: %d |MemtoReg: %d |ALUOp: %b",Branch,MemRead,MemtoReg,ALUOp);
-        // $display("MemWrite: %d |AlUSrc: %d |RegWrite %d\n",MemWrite,ALUSrc,RegWrite);
+        $display("Controle:");
+        $display("Branch %d |MemRead: %d |MemtoReg: %d |ALUOp: %b",Branch,MemRead,MemtoReg,ALUOp);
+        $display("MemWrite: %d |AlUSrc: %d |RegWrite %d\n",MemWrite,ALUSrc,RegWrite);
 
-        // $display("Registradores:");
-        // $display("Write Data: %d |DataRs1 %d |DataRs2: %d\n",WriteDataReg,data1,data2);
+        $display("Registradores:");
+        $display("Write Data: %d |DataRs1 %d |DataRs2: %d\n",WriteDataReg,data1,data2);
 
-        // $display("Gerador Imediato: %d",Imediato);
-        // $display("Instrucao: %b",instruction);
-        // $display("Saida Mux ALU %d",SaidaMuxALU);
-        // $display("ALU control: %d\n",controle_alu);
+        //$display("Gerador Imediato: %d",Imediato);
+        //$display("Instrucao: %b",instruction);
+        //$display("Saida Mux ALU %d",SaidaMuxALU);
+        //$display("ALU control: %d\n",controle_alu);
 
-        // $display("ALU:");
-        // $display("Resultado ALU: %d |Eh_zero :%d\n",resultado,eh_zero);
+        $display("ALU:");
+        $display("Resultado ALU: %d |Eh_zero :%d\n",resultado,eh_zero);
 
-        // $display("Data Memory:");
-        // $display("Endereco/Entrada: %d |WriteData/Data2: %b",resultado,data2);
-        // $display("ReadData/SaidaMemory: %b\n",ReadData);
+        $display("Data Memory:");
+        $display("Endereco/Entrada: %d |WriteData/Data2: %b",resultado,data2);
+        $display("ReadData/SaidaMemory: %b\n",ReadData);
 
-        // $display("Mux Data Memory:");
-        // $display("0-ResultadoAlu: %d |1-ReadData %d",resultado,ReadData);
-        // $display("WriteDataFinal: %d\n",WriteDataReg);
+        //$display("Mux Data Memory:");
+        //$display("0-ResultadoAlu: %d |1-ReadData %d",resultado,ReadData);
+        //$display("WriteDataFinal: %d\n",WriteDataReg);
         pc = PcProximo;
         if (pc > 52) begin
             $finish;
